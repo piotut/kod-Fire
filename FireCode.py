b3a3732d50c85533971c2e7bc3d7fa58b36e22e8
piotut <piotut@gmail.com>
2014-11-25 18:42:54 +0100

    cyclic code decoder

--- FireCode.py
+++ FireCode.py
@@ -6,7 +6,7 @@ class GF2(object):
         self.bits = bits
 
     def __add__(self, other):
-        return self.value ^ other.value
+        return GF2(self.value ^ other.value, self.bits)
 
     def __mod__(self, other):
         #print self.bits,other.bits
@@ -25,7 +25,7 @@ class GF2(object):
             ovalue = ovalue >> 1
             d = d >> 1
 
-        return actual
+        return GF2(actual, self.bits-other.bits+1)
 
 class Encoder(GF2):
     def __init__(self, codeword, generator):
@@ -35,23 +35,71 @@ class Encoder(GF2):
         
     def run(self):
         self.word.value = self.word.value << self.gen_bits-1
+        self.word.bits = self.word.bits + self.gen_bits-1 
         rest = self.word % self.gen
-        return self.word.value ^ rest
+        return self.word.value ^ rest.value
 
 class Decoder(GF2):
     def __init__(self, receive, generator):
         self.recv = receive
         self.gen = generator
+        self.zeros = 4
 
     def check_parity(self):
-        print 'Syndrom: ' + str(self.recv % self.gen)
+        print 'Syndrom: ' + bin((self.recv % self.gen).value)
         if self.recv % self.gen == 0:
             print 'Poprawne'
         else:
             print 'Wystapil blad'
+            print 'Odebrane:' + bin(self.recv.value)
+            d = 1 << self.recv.bits - 1
+            #actual = GF2(0b0, 0)
+            #actual.value = self.recv.value
+            #actual.bits = self.recv.bits
+            actual = self.recv
+            i = 0 #licznik przsuniec
+            c = GF2(0b0, 0)
+
+            syndrom = actual % self.gen
+            ones = bin(syndrom.value).count('1')
+            #print bin(syndrom), ones
+            
+            t = 1
+            while ones > t:
+                if (d & actual.value) > 0:
+                    actual.value = (actual.value ^ d) << 1
+                    c.value = c.value << 1
+                    c.value = c.value ^ 1
+                else:
+                    actual.value = actual.value << 1
+                    c.value = c.value << 1
+                    c.bits = c.bits + 1
+                syndrom = (actual + c) % self.gen
+                #print bin(actual.value), bin(self.gen.value)
+                ones = bin(syndrom.value).count('1')
+                #actual.value = actual.value ^ c.value
+            
+                #print syndrom, bin(syndrom)
+                #print '\n'
+                #print bin(actual.value+c.value)
+                #print bin(syndrom), ones
+                i = i+1
+
+            actual = actual+c+syndrom
+
+            for k in range(i):
+                if (1 & actual.value) > 0:
+                    actual.value = (actual.value >> 1) ^ (1 << actual.bits-1)
+                else:
+                    actual.value = (actual.value >> 1)
+
+            print 'Wartosc naprawiona:' + bin(actual.value)
+                    
+
+    
 
 if __name__ == '__main__':
-    a = 0b1000
+    a = 0b1101
     b = 0b1011
     c=GF2(a, 7)
     d=GF2(b, 4)
@@ -63,9 +111,13 @@ if __name__ == '__main__':
     encoder=Encoder(c, d)
     
     result = encoder.run()
-    print bin(result)
+    print 'Wyslano:' + bin(result)
 
-    decoder = Decoder(GF2(result,7), d)
+    #0b1101001
+
+    decoder = Decoder(GF2(0b1111001,7),d)#result^0b001000,7), d)
 
     decoder.check_parity()
+
+    #print GF2(0b111110, 7) % GF2(b,4)
     
